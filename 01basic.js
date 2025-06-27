ba39d024cf28af7333539fa1d00e998aebdd5a7d
yashijain88 <jainyashi854@gmail.com>
2025-06-27 16:25:39 +0530

    reverse

--- 01basic.js
+++ 01basic.js
@@ -2,6 +2,12 @@
 //     console.log(i);
 // }
 
-for (let i = 65; i <= 90; i++){             // ASCII VALUES
-    console.log(String.fromCharCode(i));
+// for (let i = 65; i <= 90; i++){             // ASCII VALUES
+//     console.log(String.fromCharCode(i));
+// }
+
+let almirah = ["yashi", "astha", "param", "ashwani", "sandeep"];
+
+for (let i = almirah.length - 1; i >= 0; i--) {
+    console.log(almirah[i]);
 }
