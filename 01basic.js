a1d1cf1c6560aa9d987042411a6c6b5f3dad3796
yashijain88 <jainyashi854@gmail.com>
2025-06-28 01:29:48 +0530

    looppractics

--- 01basic.js
+++ 01basic.js
@@ -6,8 +6,32 @@
 //     console.log(String.fromCharCode(i));
 // }
 
-let almirah = ["yashi", "astha", "param", "ashwani", "sandeep"];
+// let almirah = ["yashi", "astha", "param", "ashwani", "sandeep"];
 
-for (let i = almirah.length - 1; i >= 0; i--) {
-    console.log(almirah[i]);
+// for (let i = almirah.length - 1; i >= 0; i--) {
+//     console.log(almirah[i]);
+// }
+
+let userNumber = 10
+
+// i%2 == 1
+for(let currNum = userNumber; currNum >=1 ; currNum-=1){
+    if(currNum%2 == 1){
+        console.log(currNum);
+    }else{
+        console.log("the number is not odd", currNum);
+    }
 }
+
+// i = 9
+// 11, 9, 7-
+// i = 10, 8 , 
+
+
+
+// thisIsATestString
+// this is a test string
+
+
+// yahsiIsAGoodGirl
+// yashi is a good girl
